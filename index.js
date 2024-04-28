478a74b96eeb54c279532d0ae3370d73fc728776
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2024-04-28 16:10:24 +0530

    added html skeleton

---
+++ index.js
@@ -0,0 +1,16 @@
+/*
+A minimal design with HTML and CSS, it should look nice (no libraries, no frameworks).
+
+A text box at the top to add a todo item to the list.
+
+Add button: when a user clicks on the button, add that todo item to the list.
+
+A delete button to delete the todo item completely from the list.
+
+A checkbox to check the item off the list. (checked item should stay in the list, with some differentiation from rest of the todo items)
+
+Total number of tasks in the list.
+
+Extra points for creativity (think about user accessibility, some animations and things like that)
+ 
+ */
