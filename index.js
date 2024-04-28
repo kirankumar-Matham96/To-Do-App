e80640a60481cf60519a0f46bb939e6b99b3083d
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2024-04-28 18:37:28 +0530

    added feature to complete the tasks

--- index.js
+++ index.js
@@ -15,15 +15,29 @@ function addTask(value) {
 
   taskEl.className = "task";
 
-  taskEl.innerHTML = `<div class="task-details">
-  <label for="task-radio-${taskNumber}" class="task-data">
-  <input type="radio" class="task-radio-input" id="task-radio-${taskNumber}" />
+  taskEl.innerHTML = `<label for="${taskNumber}" class="task-data">
+  <div class="task-details" id="${taskNumber}">
+  <div class="radio"></div>
   <p>${value}</p>
-  </label>
   </div>
+  </label>
   <button class="remove-btn" onclick="removeTask()">
   <i class="fa-regular fa-circle-xmark"></i>
   </button>`;
+
+  // to stop the event propagation for the remove button
+  taskEl.querySelector(".remove-btn").addEventListener("click", (event) => {
+    event.stopPropagation();
+  });
+
+  // to strike the completed tasks
+  taskEl.addEventListener("click", () => {
+    if (!taskEl.classList.contains("stroked")) {
+      taskEl.querySelector(".radio").classList.add("bg-completed");
+      taskEl.classList.add("stroked");
+    }
+  });
+
   taskListContainerEl.appendChild(taskEl);
   mainInputEl.value = "";
 }
@@ -37,9 +51,9 @@ addBtn.addEventListener("click", () => {
 });
 
 mainInputEl.addEventListener("keydown", (event) => {
-  if (event.key == "Enter" && mainInputEl.value.trim() != "") {
-    addTask(event.target.value);
-  } else {
-    alert("Please enter something to create a task!");
+  if (event.key == "Enter") {
+    mainInputEl.value.trim() != ""
+      ? addTask(event.target.value)
+      : alert("Please enter something to create a task!");
   }
 });
